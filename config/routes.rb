39629103bcaaf367e1433596633b701f1a393f3f
Diskominfo <diskominfo@MacBook-Pro.local>
2018-12-01 00:28:39 +0800

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :users, :skip => [:registrations, :recoverable]
+
+  devise_scope :user do
+    get "/login" => "devise/sessions#new", :as => :new_user_session_path
+    post "/login" => "devise/sessions#create", :as => :user_session_path
+    delete "/logout" => "devise/sessions#destroy", :as => :destroy_user_session_path
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
